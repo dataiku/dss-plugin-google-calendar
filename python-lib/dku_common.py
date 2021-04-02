30d9d49cb80dea140f271aa912632e3d7a0468ec
Alexandre Bourret <alexandre.bourret@dataiku.com>
2021-04-02 16:56:05 +0200

    and TZ...

--- python-lib/dku_common.py
+++ python-lib/dku_common.py
@@ -32,12 +32,18 @@ def assert_no_temporal_paradox(from_date, to_date):
         if from_datetime > to_datetime:
             raise ValueError("The 'To' date currently set is before the 'From' date")
 
+
 def extract_start_end_date(event):
-    start = event.get("start", {})
-    event["start_dateTime"] = start.get("dateTime")
-    end = event.get("end", {})
-    event["end_dateTime"] = end.get("dateTime")
+    start = event.pop("start", None)
+    if start:
+        event["start_dateTime"] = start.get("dateTime")
+        event["start_timeZone"] = start.get("timeZone", "")
+    end = event.pop("end", None)
+    if end:
+        event["end_dateTime"] = end.get("dateTime")
+        event["end_timeZone"] = end.get("timeZone", "")
     return event
+
 
 def extract_start_end_dates(events):
     for event in events:
