ac0e6575a1b9df2ec2c97b877671ca984b570cea
Alex Bourret <alexandre.bourret@dataiku.com>
2026-06-29 11:57:00 +0200

    replacing google libraries with requests calls

--- python-lib/google_calendar_client.py
+++ python-lib/google_calendar_client.py
@@ -1,11 +1,10 @@
 import logging
-from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from urllib.parse import unquote
+import requests
 from dku_constants import DKUConstants as constants
 
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+CALENDAR_API_BASE_URL = "https://www.googleapis.com/calendar/v3"
 
 
 logger = logging.getLogger(__name__)
@@ -20,30 +19,28 @@ class GoogleCalendarClientError(ValueError):
 class GoogleCalendarClient():
     def __init__(self, token):
         logger.info("GoogleCalendarClient init")
-        credentials = Credentials(token, SCOPES)
-        if not credentials or not credentials.valid:
-            if credentials and credentials.expired and credentials.refresh_token:
-                credentials.refresh(Request())
-            else:
-                raise GoogleCalendarClientError("Credential not valid or need to be refreshed")
-        logger.info("Google credentials retrieved")
-        self.service = build('calendar', 'v3', credentials=credentials)
-        logger.info("Google Calendar service ok")
+        if not token:
+            raise GoogleCalendarClientError("Credential not valid or need to be refreshed")
+        self.session = requests.Session()
+        self.session.headers.update({
+            "Authorization": "Bearer {}".format(token),
+            "Accept": "application/json"
+        })
+        logger.info("Google Calendar HTTP session ready")
         self.next_page_token = None
         self.number_retrieved_events = 0
 
     def get_events(self, from_date=None, to_date=None, calendar_id=constants.DEFAULT_CALENDAR_ID, records_limit=constants.RECORDS_NO_LIMIT, can_raise=True):
-
-        kwargs = self.get_event_kwargs(from_date, to_date, calendar_id, records_limit)
+        params = self.get_event_kwargs(from_date, to_date, records_limit)
         try:
-            events_result = self.service.events().list(
-                **kwargs
-            ).execute()
-        except Exception as err:
-            error_message = "{}".format(err)
+            events_result = self._request(
+                "GET",
+                "/calendars/{}/events".format(self._encode_calendar_id(calendar_id)),
+                params=params
+            )
+        except GoogleCalendarClientError as err:
+            error_message = str(err)
             logging.error("Google Calendar client error : {}".format(error_message))
-            if error_message.startswith("<HttpError 404"):
-                error_message = "The calendar with ID '{}' does not exists.".format(calendar_id)
             self.next_page_token = None
             if can_raise:
                 raise GoogleCalendarClientError("Error: {}".format(error_message))
@@ -58,21 +55,20 @@ class GoogleCalendarClient():
 
         return events
 
-    def get_event_kwargs(self, from_date=None, to_date=None, calendar_id=constants.DEFAULT_CALENDAR_ID, records_limit=constants.RECORDS_NO_LIMIT):
-        kwargs = {
-            "calendarId": calendar_id,
+    def get_event_kwargs(self, from_date=None, to_date=None, records_limit=constants.RECORDS_NO_LIMIT):
+        params = {
             "singleEvents": True,
             "orderBy": "startTime"
         }
         if from_date:
-            kwargs["timeMin"] = from_date
+            params["timeMin"] = from_date
         if to_date:
-            kwargs["timeMax"] = to_date
+            params["timeMax"] = to_date
         if records_limit > 0:
-            kwargs["maxResults"] = records_limit
+            params["maxResults"] = records_limit
         if self.next_page_token:
-            kwargs["pageToken"] = self.next_page_token
-        return kwargs
+            params["pageToken"] = self.next_page_token
+        return params
 
     def update_next_page_token(self, events_result, records_limit=constants.RECORDS_NO_LIMIT):
         if records_limit == constants.RECORDS_NO_LIMIT or self.number_retrieved_events < records_limit:
@@ -100,5 +96,48 @@ class GoogleCalendarClient():
             'dateTime': kwargs.get("end")
         }
 
-        response = self.service.events().insert(calendarId=calendar_id, body=event).execute()
+        response = self._request(
+            "POST",
+            "/calendars/{}/events".format(self._encode_calendar_id(calendar_id)),
+            json=event
+        )
         return response.get("htmlLink")
+
+    def _request(self, method, path, params=None, json=None):
+        url = "{}{}".format(CALENDAR_API_BASE_URL, path)
+        try:
+            response = self.session.request(method, url, params=params, json=json, timeout=30)
+        except requests.RequestException as err:
+            raise GoogleCalendarClientError(str(err))
+
+        if response.ok:
+            return response.json()
+
+        error_message = self._build_error_message(response)
+        raise GoogleCalendarClientError(error_message)
+
+    def _build_error_message(self, response):
+        try:
+            payload = response.json()
+        except ValueError:
+            payload = {}
+
+        error = payload.get("error", {})
+        if response.status_code == 404:
+            return "The calendar with ID '{}' does not exists.".format(
+                self._extract_calendar_id_from_response(response)
+            )
+
+        message = error.get("message") or response.text or "HTTP {}".format(response.status_code)
+        return "HTTP {}: {}".format(response.status_code, message)
+
+    def _extract_calendar_id_from_response(self, response):
+        marker = "/calendars/"
+        path = response.request.path_url
+        if marker not in path:
+            return constants.DEFAULT_CALENDAR_ID
+        encoded_calendar_id = path.split(marker, 1)[1].split("/events", 1)[0]
+        return unquote(encoded_calendar_id)
+
+    def _encode_calendar_id(self, calendar_id):
+        return requests.utils.quote(calendar_id, safe="")
